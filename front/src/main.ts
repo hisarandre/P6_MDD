cbf72d86d09a61114014f6e189c336cfb43057d7
Sara Andre <hisarandre@gmail.com>
2025-08-31 11:24:43 +0900

    Update jwt interceptor

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,22 +3,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import {jwtInterceptor} from "./app/core/interceptors/jwt.interceptor";
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
     ...(appConfig.providers ?? []),
-    provideHttpClient(withInterceptors([
-      (req, next) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-          req = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
-        }
-        console.log('Intercepted request', req.url, 'Token:', token);
-        return next(req);
-      }
-    ])),
-    provideAnimations(),
+    provideHttpClient(withInterceptors([jwtInterceptor])),
   ]
 })
   .catch(err => console.error(err));
